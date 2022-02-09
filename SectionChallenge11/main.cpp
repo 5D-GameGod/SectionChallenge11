ea275210ce77bed0a00dd9abf8b9e5933e03f491
pcnnadi <61913485+pcnnadi@users.noreply.github.com>
2022-02-09 14:44:39 +0000

    Modified GetChoice function to follow up with different operations depending on the user's input

--- SectionChallenge11/main.cpp
+++ SectionChallenge11/main.cpp
@@ -3,11 +3,14 @@
 
 void DisplayMenu();
 void GetChoice();
+void PrintNumbers();
+
 
 char Choice{};
 
 int main()
 {
+	std::vector<int> NumberList;
 	DisplayMenu();
 	return 0;
 }
@@ -33,4 +36,41 @@ void DisplayMenu()
 void GetChoice()
 {
 	std::cin >> Choice;
+
+	if (Choice == 'P' || Choice == 'p')
+	{
+		PrintNumbers();
+	}
+
+	/*else if (Choice == 'A' || Choice == 'a')
+	{
+		AddNumbers();
+	}
+	else if (Choice == 'M' || Choice == 'm')
+	{
+		CalculateMean();
+	}
+	else if (Choice == 'S' || Choice == 's')
+	{
+		DetermineSmallest();
+	}
+	else if (Choice == 'L' || Choice == 'l')
+	{
+		DetermineLargest();
+	}*/
+	else if (Choice == 'Q' || Choice == 'q')
+	{
+		std::cout << "Goodbye";
+	}
+	else
+		std::cout << "***Unknown selection, please try again***" << std::endl;
+}
+
+void PrintNumbers()
+{
+	if(NumberList.length != 0)
+	{
+
+	}
+	std::cout << "[] - the list is empty." << std::endl;
 }
