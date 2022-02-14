d0b6132883df7511a4a442b8fda01123d49b2b7b
pcnnadi <61913485+pcnnadi@users.noreply.github.com>
2022-02-14 16:24:56 +0000

    Completed the task but haven't implemented a working input check yet.

--- SectionChallenge11/main.cpp
+++ SectionChallenge11/main.cpp
@@ -2,75 +2,162 @@
 #include<vector>
 
 void DisplayMenu();
-void GetChoice();
-void PrintNumbers();
+char GetChoice();
 
-
-char Choice{};
+void PrintNumbers(const std::vector<int>& v);
+void AddNumber(std::vector<int>& v);
+//void InputCheck(int& i, std::vector<int>& v);
+void CalculateMean(const std::vector<int>& v);
+void DetermineSmallest(const std::vector<int>& v);
+void DetermineLargest(const std::vector<int>& v);
 
 int main()
 {
 	std::vector<int> NumberList;
-	DisplayMenu();
+	char Choice{};
+
+	do
+	{
+		DisplayMenu();
+		Choice = GetChoice();
+
+		if (Choice == 'P')
+		{
+			PrintNumbers(NumberList);
+		}
+		else if (Choice == 'A')
+		{
+			//std::cout << "Please enter the number you wish to add to the list: ";
+			AddNumber(NumberList);
+		}
+		else if (Choice == 'M')
+		{
+			CalculateMean(NumberList);
+		}
+		else if (Choice == 'S')
+		{
+			DetermineSmallest(NumberList);
+		}
+		else if (Choice == 'L')
+		{
+			DetermineLargest(NumberList);
+		}
+		else if (Choice == 'Q')
+		{
+			std::cout << "Goodbye...";
+		}
+		else
+			std::cout << "Unknown option entered. Please try again.\n";
+	} while (Choice != 'Q' && Choice != 'q');
+	std::cout << std::endl;
+
 	return 0;
 }
 
 void DisplayMenu()
 {
-	do 
-	{
-		std::cout << "\nChoose which action you want to perform." << std::endl;
-		std::cout << "\nP - Print numbers." << std::endl;
-		std::cout << "A - Add a number." << std::endl;
-		std::cout << "M - Display mean of the numbers." << std::endl;
-		std::cout << "S - Display the smallest number." << std::endl;
-		std::cout << "L - Display the largest number." << std::endl;
-		std::cout << "Q - Quit program." << std::endl;
-		std::cout << "\nEnter your choice: ";
+	std::cout << "P - Print numbers" << std::endl;
+	std::cout << "A - Add a number" << std::endl;
+	std::cout << "M - Display mean of the numbers" << std::endl;
+	std::cout << "S - Display the smallest number" << std::endl;
+	std::cout << "L - Display the largest number" << std::endl;
+	std::cout << "Q - Quit" << std::endl;
 
-		GetChoice();
-	}
-	while (Choice != 'Q' && Choice != 'q');
+	std::cout << "\nEnter your choice: ";
 }
 
-void GetChoice()
+char GetChoice()
 {
+	char Choice{};
 	std::cin >> Choice;
+	return toupper(Choice);
+}
 
-	if (Choice == 'P' || Choice == 'p')
+void PrintNumbers(const std::vector<int>& v)
+{
+	if (v.size() != 0)
 	{
-		PrintNumbers();
-	}
-
-	/*else if (Choice == 'A' || Choice == 'a')
-	{
-		AddNumbers();
-	}
-	else if (Choice == 'M' || Choice == 'm')
-	{
-		CalculateMean();
-	}
-	else if (Choice == 'S' || Choice == 's')
-	{
-		DetermineSmallest();
-	}
-	else if (Choice == 'L' || Choice == 'l')
-	{
-		DetermineLargest();
-	}*/
-	else if (Choice == 'Q' || Choice == 'q')
-	{
-		std::cout << "Goodbye";
+		std::cout << "[";
+		for (int i{ 0 }; i < v.size(); i++)
+		{
+			std::cout << v.at(i) << " ";
+		}
+		std::cout << "]" << std::endl;
 	}
 	else
-		std::cout << "***Unknown selection, please try again***" << std::endl;
+		std::cout << "[] - the list is empty." << std::endl;       
 }
 
-void PrintNumbers()
+void AddNumber(std::vector<int> &v)
 {
-	if(NumberList.length != 0)
-	{
+	std::cout << "Enter the number you wish to add to the list: ";
+	int input{};
+	std::cin >> input;
+	//InputCheck(input,v);
+	v.push_back(input);
+	std::cout << input << " has been added to the list." << std::endl;
+}
 
+//void InputCheck(int &i, std::vector<int> &v)
+//{
+//	if (isdigit(i) == 1)
+//	{
+//		std::cout << "Valid Input.\n" << std::endl;
+//	}
+//	else
+//		std::cout << "Invalid Input. Please try again." << std::endl;
+//		AddNumber(v);
+//} 
+
+void CalculateMean(const std::vector<int> &v)
+{
+	if (v.size() != 0)
+	{
+		double Mean{ 0 };
+		int sum{ 0 };
+		for(auto num:v)
+		{
+			sum += num;
+		}
+		Mean = (sum) / v.size();
+		std::cout << "The mean is " << Mean << std::endl;
 	}
-	std::cout << "[] - the list is empty." << std::endl;
+	else
+		std::cout << "Unable to calculate mean - the list is empty." << std::endl;
+}
+
+void DetermineSmallest(const std::vector<int>& v)
+{
+	if(v.size() != 0)
+	{
+		int smallest{ v.at(0) };
+		for(int i{0}; i < v.size(); i++)
+		{
+			if(smallest >= v.at(i))
+			{
+				smallest = v.at(i);
+			}
+		}
+		std::cout << "The smallest number in the list is " << smallest << std::endl;
+	}
+	else
+		std::cout << "Unable to determine the smallest number - the list is empty." << std::endl;
+}
+
+void DetermineLargest(const std::vector<int>& v)
+{
+	if (v.size() != 0)
+	{
+		int largest{ v.at(0) };
+		for (int i{ 0 }; i < v.size(); i++)
+		{
+			if (largest <= v.at(i))
+			{
+				largest = v.at(i);
+			}
+		}
+		std::cout << "The smallest number in the list is " << largest << std::endl;
+	}
+	else
+		std::cout << "Unable to determine the smallest number - the list is empty." << std::endl;
 }
